1af668a022faedd6aa432623c88e88cfb9522ee3
Stevie Gillen <stevie_gillen@hotmail.com>
2026-01-23 22:24:35 +0000

    Use char to store and use single characters

---
+++ 02 Variables/Practice/Grades.java
@@ -0,0 +1,15 @@
+public class Grades {
+    public static void main(String[] args) {
+        /*
+        the 'char' data type uses 2 bytes of memory
+        char variables must be surrounded with single quotes e.g. 'A'
+        */
+       char potions = 'A';
+       char arithmancy = 'B';
+       char charms = 'C';
+
+       System.out.println("Potions: " + potions);
+       System.out.println("Arithmancy: " + arithmancy);
+       System.out.println("Charms: " + charms);
+    }
+}
