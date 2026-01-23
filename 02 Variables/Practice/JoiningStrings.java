1341fa867f9d9977814afa57a25882b5e1e71c9a
Stevie Gillen <stevie_gillen@hotmail.com>
2026-01-23 22:16:42 +0000

    Create and join strings

---
+++ 02 Variables/Practice/JoiningStrings.java
@@ -0,0 +1,10 @@
+public class JoiningStrings {
+    public static void main(String[] args){
+        //  practice with strings
+        int year = 2010;
+        String winner = "Spain";
+        String announcement = "The Winner of the " + year + " World Cup was " + winner;
+
+        System.out.println(announcement);
+    }
+}
