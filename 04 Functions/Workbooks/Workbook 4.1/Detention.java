655e521efe2235f5e0aff5ceaa4d4c70c7a046fd
Stevie Gillen <stevie_gillen@hotmail.com>
2026-02-01 21:14:29 +0000

    Complete workbook 4.1

---
+++ 04 Functions/Workbooks/Workbook 4.1/Detention.java
@@ -0,0 +1,18 @@
+public class Detention {
+    public static void main(String[] args) {
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+   }
+
+   public static void printLines() {
+        //what Bart did so far
+        System.out.println("I will not copy and paste code.");
+        System.out.println("I will use PascalCase when writing class names.");
+        System.out.println("I will use camelCase when writing function names.");
+        System.out.println("I will use camelCase when writing variables names.\n");
+   }
+}
